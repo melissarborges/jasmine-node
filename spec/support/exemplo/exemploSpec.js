4e0857aa228ec38e6c063540cf9563f36be23429
C1323471 Melissa Rezende Borges <c1323471@interno.bb.com.br>
2022-01-25 18:23:51 -0300

    Arquivos do projeto local

---
+++ spec/support/exemplo/exemploSpec.js
@@ -0,0 +1,11 @@
+describe("Suite de teste de exemplo com NodeJs", function(){
+
+    it("deve demonstrar o uso do Jasmine com o NodeJS", function(){
+        expect(true).toEqual(true);
+    });
+    it("Deve demosntrar o uso do karma em mode de escuta", function(){
+        expect(1).toEqual(1);
+});
+
+  
+});
